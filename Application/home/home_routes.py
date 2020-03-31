f29557f668ed32faffc7a9d8995ee0f731fdfd05
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2020-03-31 18:39:51 +0200

    Add new tables

--- Application/home/home_routes.py
+++ Application/home/home_routes.py
@@ -1,9 +1,7 @@
-from flask import render_template, request, make_response, Blueprint
+from flask import render_template, Blueprint
 from flask_login import login_required
 
-from Application import db
-from Application.models.FormModel import FormModel
-from Application.models.users import User
+from Application.models.UserTable import User
 
 home_bp = Blueprint("home_bp", __name__, template_folder="templates", static_folder="static")
 
@@ -11,15 +9,5 @@ home_bp = Blueprint("home_bp", __name__, template_folder="templates", static_fol
 @home_bp.route("/", methods=["GET"])
 @login_required
 def create_image():
-    name = request.args.get("name")
-    if name:
-        existing_name = FormModel.query.filter(FormModel.name == name).first()
-        if existing_name:
-            return make_response(f"{name} already exist")
-        new_form_model = FormModel(name=name)
-        db.session.add(new_form_model)
-        db.session.commit()
-
-    forms = FormModel.query.all()
     users = User.query.all()
-    return render_template("home.html", forms=forms, users=users, title="Show FormModel")
+    return render_template("home.html", users=users, title="Show FormModel")
---
+++ Application/models/BillTable.py
@@ -0,0 +1,19 @@
+from Application import db
+from .ClientTable import Client
+from .UserTable import User
+
+
+class Bill(db.Model):
+    __tablename__ = "Bills"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    user = db.column(db.Integer, db.ForeignKey(User.id), nullable=False)
+    client = db.column(db.Integer, db.ForeignKey(Client.id), nullable=False)
+
+    number = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    expiration = db.Column(db.Date, nullable=False)
+    price = db.Column(db.Float(precision=2), nullable=False)
+    cash = db.Column(db.Boolean, nullable=False)
+    paid = db.Column(db.Boolean, nullable=False)
