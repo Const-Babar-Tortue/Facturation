566bd9a007462f06edb63254b8f1e10d49f7d047
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2020-04-01 00:44:12 +0200

    yes

--- Application/api/bills/bill_route.py
+++ Application/api/bills/bill_route.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify
-from flask_jwt import jwt_required
+from flask_jwt import jwt_required, current_identity
 
 from Application.models.BillTable import Bill
 
@@ -10,5 +10,24 @@ bill_api_bp = Blueprint("bill_api_bp", __name__)
 @bill_api_bp.route("/bill", methods=["GET"])
 @jwt_required()
 def bill():
-    results = Bill.query.all()
-    return jsonify(results)
+    user_id = current_identity.id
+    results = Bill.query.filter(Bill.user_id == user_id).all()
+
+    # date = datetime.datetime.now().date()
+    # babar = Bill(id=1, user_id=1, client_id=1, number=1,
+    #              date=date, expiration=date, price=1, cash=True, paid=False)
+    # db.session.add(babar)
+    # db.session.commit()
+
+    response = list()
+    for res in results:
+        item = {
+            'number': res.number,
+            'date': res.date,
+            'expiration': res.expiration,
+            'price': res.price,
+            'paid': res.paid
+        }
+        response.append(item)
+
+    return jsonify(response)
