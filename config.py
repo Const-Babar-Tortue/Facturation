9663f200eb8f2f9c6dcd8e371f2685984c7769d3
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2020-03-31 18:20:49 +0200

    Fix nathan's huge mistakes

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ class Config:
     FLASK_DEBUG = environ.get("FLASK_DEBUG")
     SECRET_KEY = environ.get("SECRET_KEY")
 
-    SQLALCHEMY_DATABASE_URI = environ.get("URI")
+    SQLALCHEMY_DATABASE_URI = sqlite:///./data.db
     SQLALCHEMY_TRACK_MODIFICATIONS = environ.get("TRACK_MODIFICATION")
