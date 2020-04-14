c0cf89f5b2ce609c869af074e2f0ea852ec88f1d
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2020-04-14 02:37:47 +0200

    Remove script, is now in Makefile

--- api-doc/gendoc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-blueprint_src=api.apib
-output=../docs/index.html
-template=default
-theme=cyborg
-
-aglio -i "$blueprint_src" -o "$output" --theme-template=$template --theme-variables=$theme
