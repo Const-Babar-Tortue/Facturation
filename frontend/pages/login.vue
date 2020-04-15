011be319ccf1de6c013e4c8380cd70a54d526695
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2020-04-15 23:14:46 +0200

    Add login service and page, receive JWT token

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,9 +1,85 @@
 <template>
-    <b-container>
-        <b-button>Login</b-button>
-    </b-container>
+    <Centered>
+        <b-card header="Create an account">
+            <ValidationObserver ref="observer" v-slot="{ passes }">
+                <b-form @submit.prevent="passes(onSubmit)">
+
+                    <b-alert :show="invalid" variant="danger" dismissible class="mt-3">
+                        Invalid credentials
+                    </b-alert>
+
+                    <b-alert :show="error" variant="danger" dismissible class="mt-3">
+                        An error occurred while registering
+                    </b-alert>
+
+
+                    <BTextInputWithValidation
+                        rules="required"
+                        type="text"
+                        label="Username:"
+                        name="Username"
+                        v-model="username"
+                        placeholder="Enter a username"
+                    />
+
+                    <BTextInputWithValidation
+                        rules="required"
+                        name="Password"
+                        vid="password"
+                        type="password"
+                        label="Password"
+                        v-model="password"
+                        description="We'll never share your password with anyone else"
+                        placeholder="Enter password"
+                    />
+
+                    <b-button type="submit" variant="primary">Submit</b-button>
+                </b-form>
+            </ValidationObserver>
+        </b-card>
+    </Centered>
 </template>
 
 <script>
-    export default {}
+    import {extend, ValidationObserver} from "vee-validate";
+    import {required} from 'vee-validate/dist/rules';
+    import Centered from '@/components/Centered'
+    import BTextInputWithValidation from '@/components/BTextInputWithValidation'
+    import LoginService from '@/services/LoginService.js'
+
+    extend('required', {
+        ...required,
+        message: 'The {_field_} field is required'
+    });
+
+    export default {
+        name: "Login",
+        components: {
+            Centered,
+            ValidationObserver,
+            BTextInputWithValidation
+        },
+        data: () => ({
+            username: null,
+            password: null,
+            invalid: false,
+            error: false
+        }),
+        methods: {
+            onSubmit() {
+                LoginService.login({
+                    username: this.username,
+                    password: this.password
+                }).then(({token}) => {
+                        console.log(token)
+                        // return this.$router.push('/');
+                    }
+                ).catch(e => {
+                    if (e.invalid) this.invalid = true
+                    else this.error = true
+                })
+            },
+
+        }
+    }
 </script>
--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,6 +1,13 @@
 export const state = () => ({
+    token: null
 })
 
 export const mutations = {
+    setToken(state, token) {
+        state.token = token
+    }
+}
+
+export const getters = {
 
 }
