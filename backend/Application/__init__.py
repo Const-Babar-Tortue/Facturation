288f522a20847a9188dc3561468c5a05996908d3
Nathan Henry <na.henry@outlook.com>
2020-04-22 09:33:30 +0200

    Added new function to api

--- backend/Application/__init__.py
+++ backend/Application/__init__.py
@@ -10,6 +10,7 @@ db = SQLAlchemy()
 from .api.register import Register
 from .api.clients import Clients
 from .api.getNameClient import GetNameClient
+from .api.deleteClient import DeleteClient
 
 app = Flask(__name__, instance_relative_config=False)
 api = Api(app)
@@ -30,5 +31,6 @@ def init_app():
     api.add_resource(Register, '/register')
     api.add_resource(Clients, '/clients')
     api.add_resource(GetNameClient, '/clients/names')
+    api.add_resource(DeleteClient, '/clients/delete')
 
     return app
