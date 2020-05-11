6a04644fd1b08812e08fcbb813fa4d20be8d4add
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2020-05-11 22:24:44 +0200

    Add bills link inside navbar

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -3,6 +3,7 @@
         <b-navbar-nav>
             <b-nav-item to="/">Home</b-nav-item>
             <b-nav-item v-if="isAuthenticated" to="/clients">Clients</b-nav-item>
+            <b-nav-item v-if="isAuthenticated" to="/bills">Bills</b-nav-item>
         </b-navbar-nav>
 
         <b-navbar-nav class="ml-auto">
