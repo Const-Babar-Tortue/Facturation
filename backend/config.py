0b088784e8f6bdd7fc6c48e036ebf56df139e2cd
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2020-05-13 20:40:04 +0200

    fix a bug..

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,5 @@
 from os import environ
+from datetime import timedelta
 
 
 class Config:
@@ -7,7 +8,7 @@ class Config:
     FLASK_DEBUG = environ.get("FLASK_DEBUG")
     SECRET_KEY = environ.get("SECRET_KEY")
     JWT_AUTH_HEADER_PREFIX = 'Bearer'
-    JWT_EXPIRATION_DELTA = 604800
+    JWT_EXPIRATION_DELTA = timedelta(seconds=604800)
 
     SQLALCHEMY_DATABASE_URI = 'sqlite:///./data.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = environ.get("TRACK_MODIFICATION")
