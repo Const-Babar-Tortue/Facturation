30a9b22cd2fd2079de8e8c57657c83753b52a316
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2020-05-16 01:28:37 +0200

    More e2e tests

--- frontend/tests/index.js
+++ frontend/tests/index.js
@@ -1,7 +1,14 @@
+require('dotenv').config()
+const username = process.env.USERNAME
+const password = process.env.PASSWORD
+
+// const baseUrl = 'http://127.0.0.1:3002'
+const baseUrl = 'https://facturesoft.ovh'
+
 module.exports = {
     'Test title'(browser) {
         browser
-            .url('http://127.0.0.1:3002')
+            .url(baseUrl)
             .waitForElementVisible('body')
             .assert.titleContains('facturesoft')
             .end()
@@ -9,7 +16,7 @@ module.exports = {
 
     'Test login with invalid credentials'(browser) {
         browser
-            .url('http://127.0.0.1:3002/login')
+            .url(`${baseUrl}/login`)
             .waitForElementVisible('body')
             .setValue('input[name=Username]', 'babar')
             .setValue('input[name=Password]', 'michel')
@@ -17,6 +24,22 @@ module.exports = {
             .click('button[type=submit]')
             .assert.visible('.alert')
             .assert.containsText('.alert', 'Invalid credentials')
+            .end()
+    },
+
+    'Test login with valid credentials'(browser) {
+        browser
+            .url(`${baseUrl}/login`)
+            .waitForElementVisible('body')
+            .setValue('input[name=Username]', username)
+            .setValue('input[name=Password]', password)
+            .assert.visible('button[type=submit]')
+            .click('button[type=submit]')
+            .assert.not.elementPresent('.alert')
+            .assert.urlEquals(`${baseUrl}/`)
+            .assert.containsText('button[type=button]', 'Logout')
+            .assert.containsText('a[href="/clients"]', 'Clients')
+            .assert.containsText('a[href="/bills"]', 'Bills')
             .end()
     },
 }
