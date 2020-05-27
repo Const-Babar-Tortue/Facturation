b09e0873a86e8ce614b56164fc7b0f6d401184aa
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2020-05-27 18:03:21 +0200

    Add client info modal popup

--- backend/Application/api/clients/singleclient.py
+++ backend/Application/api/clients/singleclient.py
@@ -22,3 +22,23 @@ class SingleClient(Resource):
         db.session.commit()
 
         return {"message": "Deleted"}
+
+    def get(self, client_id):
+        client = Client.query.filter(Client.id == client_id).first()
+
+        if client is None:
+            return {"message": "Client does not exists"}, 404
+
+        client_dict = {
+            "id": client.id,
+            "name": client.name,
+            "street": client.street,
+            "streetNumber": client.street_number,
+            "postalCode": client.postal_code,
+            "city": client.city,
+            "firm": client.firm,
+            "vatNumber": client.vat_number,
+        }
+
+        return client_dict
+
--- frontend/pages/bills/index.vue
+++ frontend/pages/bills/index.vue
@@ -2,6 +2,52 @@
     <b-container>
         <DataTable title="Bills" destination="/bills/create" :items="bills">
             <template v-slot>
+                <b-modal id="info-modal" hide-footer>
+                    <div class="d-block text-center">
+                        <h3>Info</h3>
+                        <h2>{{ info.subject }}</h2>
+                    </div>
+                    <b-list-group>
+                        <b-list-group-item>ID: {{ info.id }}</b-list-group-item>
+                        <b-list-group-item>
+                            Cash: {{ info.cash }}
+                        </b-list-group-item>
+                        <b-list-group-item>
+                            Date: {{ info.date }}
+                        </b-list-group-item>
+                        <b-list-group-item>
+                            Date: {{ info.expiration }}
+                        </b-list-group-item>
+                        <b-list-group-item>
+                            Date: {{ info.paid }}
+                        </b-list-group-item>
+                        <b-list-group-item>
+                            Date: {{ info.price }}
+                        </b-list-group-item>
+                    </b-list-group>
+                    <div class="d-block text-center">
+                        <h2>Client</h2>
+                    </div>
+                    <b-list-group>
+                        <b-list-group-item>
+                            ID: {{ client.id }}
+                        </b-list-group-item>
+                        <b-list-group-item>
+                            Name: {{ client.name }}
+                        </b-list-group-item>
+                        <b-list-group-item>
+                            City: {{ client.postalCode }}, {{ client.city }}
+                        </b-list-group-item>
+                        <b-list-group-item>
+                            Address: {{ client.city }},
+                            {{ client.streetNumber }}
+                        </b-list-group-item>
+                        <b-list-group-item>
+                            VAT number: {{ client.vatNumber }}
+                        </b-list-group-item>
+                    </b-list-group>
+                </b-modal>
+
                 <b-table
                     class="m-0"
                     responsive=""
@@ -19,6 +65,9 @@
                     <template v-slot:cell(actions)="row">
                         <b-button size="sm" @click="toggle(row.item)">
                             Toggle
+                        </b-button>
+                        <b-button size="sm" @click="showInfo(row.item)">
+                            More info
                         </b-button>
                         <b-button size="sm" @click="deleteBill(row.item)">
                             Delete
@@ -39,6 +88,8 @@ export default {
     components: { DataTable },
     data: () => ({
         selectedItem: null,
+        info: '',
+        client: '',
         fields: [
             { key: 'id', sortable: true },
             { key: 'subject', sortable: true },
@@ -57,6 +108,13 @@ export default {
     },
     methods: {
         ...mapActions({ deleteBill: 'bills/delete', toggle: 'bills/toggle' }),
+        showInfo(item) {
+            this.info = item
+            this.$axios
+                .get(`/clients/${item.clientId}`)
+                .then(({ data }) => (this.client = data))
+            this.$bvModal.show('info-modal')
+        },
     },
     head: () => ({
         title: 'Bills',
